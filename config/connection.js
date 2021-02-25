ba835d1d659ee764b3066023fd4b801a1e633681
jxhnkndl <jkroyston@gmail.com>
2021-02-25 16:16:04 -0500

    Configure and export connection to database

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Import modules
+const mysql = require('mysql');
+
+// Config database connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  // Note that password has been set to 'password' for
+  // assignments that require credentials be available
+  // in a public repository
+  password: 'password',
+  database: 'burgers_db'
+});
+
+// Establish database connection
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connection at id ${connection.threadId}`);
+});
+
+// Export connection object
+module.exports = connection;
