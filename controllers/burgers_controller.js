ae32bfe3265a07960f560e966756784c46860ba3
jxhnkndl <jkroyston@gmail.com>
2021-02-26 17:42:24 -0500

    Create and test API POST route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,7 +10,18 @@ const burger = require('../models/burger');
 
 // Get all burgers
 router.get('/', (req, res) => {
-  burger.all((result) => res.json(result));
+  burger.all((result) => {
+    
+    res.json(result);
+
+  });
+});
+
+// Add new burger
+router.post('/api/burgers', (req, res) => {
+  burger.create(req.body, (result) => {
+    res.status(201).json({ id: result.insertId });
+  });
 });
 
 // Export router
