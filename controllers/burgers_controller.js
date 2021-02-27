afcd5c537f050f39c4e07e9fc9e44621374e8b03
jxhnkndl <jkroyston@gmail.com>
2021-02-27 17:52:05 -0500

    Connect dynamically generated index view with application GET all route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,16 +11,24 @@ const burger = require('../models/burger');
 // Get all burgers
 router.get('/', (req, res) => {
   burger.all((result) => {
-    res.json(result);
+    // Format burger for use in template
+    const burgerObj = {
+      burgers: result,
+    };
+
+    console.log(burgerObj);
+
+    // Return dynamically generated index view
+    res.render('index', burgerObj);
   });
 });
 
 // Add new burger
 router.post('/api/burgers', (req, res) => {
   burger.create(req.body, (result) => {
-    res.status(201).json({ 
+    res.status(201).json({
       success: true,
-      id: result.insertId 
+      id: result.insertId,
     });
   });
 });
@@ -29,7 +37,7 @@ router.post('/api/burgers', (req, res) => {
 router.put('/api/burgers/:id', (req, res) => {
   burger.update(req.body, req.params, (result) => {
     if (result.affectedRows === 0) {
-      res.status(404).json({ success: false })
+      res.status(404).json({ success: false });
     }
 
     res.status(200).json({ success: true });
